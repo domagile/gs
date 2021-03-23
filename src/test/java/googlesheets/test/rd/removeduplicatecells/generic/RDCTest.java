91aeaedce132dd6ccc2e6bf5628d4705b08437da
smartfamilyapp <smartfamilyapp@gmail.com>
2021-03-23 15:05:24 +0300

    AUTOMATION-1 - RD Remove duplicate cells - Framework basics

---
+++ src/test/java/googlesheets/test/rd/removeduplicatecells/generic/RDCTest.java
@@ -0,0 +1,42 @@
+package googlesheets.test.rd.removeduplicatecells.generic;
+
+import googlesheets.service.technical.file.FileType;
+import googlesheets.test.SpreadsheetTest;
+
+import java.io.IOException;
+
+import static googlesheets.service.technical.file.FileService.compareFileWithEtalon;
+import static googlesheets.service.technical.file.FileService.removeDownloadedListFile;
+import static googlesheets.service.GoogleSheetService.*;
+
+public abstract class RDCTest extends SpreadsheetTest {
+    @Override
+    protected String getSpreadsheetName() {
+        return getSpreadsheetName(getClass());
+    }
+
+
+    protected void checkResult(String listName, String etalonFileName) throws IOException {
+        //fixme: refactor to invoke generic checks from checkResult()
+        startCSVDownload();
+        sleep(2000);
+        restoreInitialDocumentState(listName);
+        compareFileWithEtalon(getSpreadsheetName(), listName, etalonFileName);
+        removeDownloadedListFile(getSpreadsheetName(), listName, FileType.CSV);
+    }
+
+    protected void checkExcelResult(String listName, String etalonFileName) throws IOException {
+        //fixme: refactor to invoke generic checks from checkResult()
+        startXLSXDownload();
+        sleep(2000);
+        restoreInitialDocumentState(listName);
+        compareFileWithEtalon(getSpreadsheetName(), listName, etalonFileName, FileType.XLSX);
+        removeDownloadedListFile(getSpreadsheetName(), listName, FileType.XLSX);
+    }
+
+
+    protected void restoreInitialDocumentState(String resultListName)  {
+        clickUndo(2);
+    }
+}
+
