dbd189336c2eab78b25b6156e13a84059dc8078c
Olga <domagile@gmail.com>
2021-03-29 16:02:12 +0300

    AUTOMATION-24 Combine sheets - Framework basics

---
+++ src/test/java/googlesheets/test/rd/generic/RDTest.java
@@ -0,0 +1,54 @@
+package googlesheets.test.rd.generic;
+
+import googlesheets.service.generic.addon.ResultInfo;
+import googlesheets.service.technical.file.FileType;
+import googlesheets.test.SpreadsheetTest;
+
+import java.io.IOException;
+
+import static googlesheets.service.generic.google.GoogleSheetService.*;
+import static googlesheets.service.technical.file.FileService.compareFileWithEtalon;
+import static googlesheets.service.technical.file.FileService.removeDownloadedListFile;
+
+public class RDTest extends SpreadsheetTest {
+    protected void checkResult(String listName, String etalonFileName) {
+        //fixme: refactor to invoke generic checks from checkResult()
+        startCSVDownload();
+        sleep(2000);
+        restoreInitialDocumentState(listName);
+        compareFileWithEtalon(getSpreadsheetName(), listName, etalonFileName);
+        removeDownloadedListFile(getSpreadsheetName(), listName, FileType.CSV);
+    }
+
+
+    protected void restoreInitialDocumentState(String listName) {
+    }
+
+
+    protected void checkExcelResult(String listName, String etalonFileName) throws IOException {
+        //fixme: refactor to invoke generic checks from checkResult()
+        startXLSXDownload();
+        sleep(2000);
+        restoreInitialDocumentState(listName);
+        compareFileWithEtalon(getSpreadsheetName(), listName, etalonFileName, FileType.XLSX);
+        removeDownloadedListFile(getSpreadsheetName(), listName, FileType.XLSX);
+    }
+
+    protected void checkNewSpreadsheetResult(String sourceListName, String etalonFileName, ResultInfo resultInfo) throws IOException {
+        restoreInitialStateForNewSpreadsheetOption();
+        sleep(1000);
+        openDocument(resultInfo.getNewSpreadsheetLink());
+        startCSVDownload();
+        sleep(2000);
+        String listName = getResultListName(sourceListName);
+        moveSpreadsheetToTrash();
+        String newSpreadsheetName = String.format("%s - %s", getSpreadsheetName(), listName.substring(listName.indexOf('-') + 2));
+        compareFileWithEtalon(newSpreadsheetName, listName, etalonFileName);
+        removeDownloadedListFile(newSpreadsheetName, listName, FileType.CSV);
+    }
+
+
+    protected void restoreInitialStateForNewSpreadsheetOption()
+    {
+    }
+}
