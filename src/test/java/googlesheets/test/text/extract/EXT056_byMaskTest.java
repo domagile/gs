c2b2d67d19568398e5078b5b781f55ec7f671749
smartfamilyapp <smartfamilyapp@gmail.com>
2021-05-30 10:46:38 +0300

    AUTOMATION-94 EXT Extract text - Test cases 51-70

--- src/test/java/googlesheets/test/text/extract/EXT056_byMaskTest.java
+++ src/test/java/googlesheets/test/text/extract/EXT056_byMaskTest.java
@@ -5,10 +5,11 @@ import googlesheets.model.text.extract.ExtractTextOptions;
 import googlesheets.model.text.extract.enums.ExtractTypeEnumeration;
 import org.junit.Test;
 
+import static googlesheets.service.generic.google.GoogleSheetService.clickUndo;
+
 public class EXT056_byMaskTest extends EXTTest {
     @Test
-    public void extract()
-    {
+    public void extract() {
         ExtractTextOptions options = new ExtractTextOptionBuilder()
                 .range("L3:L20")
                 .extractType(ExtractTypeEnumeration.BY_MASK)
@@ -18,4 +19,10 @@ public class EXT056_byMaskTest extends EXTTest {
         execute(options);
         checkResult();
     }
+
+    @Override
+    protected void restoreInitialDocumentState() {
+        clickUndo(2);
+    }
+
 }
